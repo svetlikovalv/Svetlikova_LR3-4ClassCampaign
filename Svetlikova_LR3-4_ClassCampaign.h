9470914f1e48b41b857319c243386b23c8ede0f2
svetlikovalv <svetlikova13@gmail.com>
2025-03-22 12:36:29 +0300

    что-то натворил

--- Svetlikova_LR3-4_ClassCampaign.h
+++ Svetlikova_LR3-4_ClassCampaign.h
@@ -2,4 +2,37 @@
 #define CAMPAIGN
 
 #include <iosteram>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+class Campaign{
+    string name;//название фирмы
+    unsigned budget;//доходы-расходы
+    unsigned cost;//затраты для вычисления ROI
+    vector<double>results;//рейтинги фирм
+public:
+    Campaign();//по умолчанию
+    Campaign(unsigned r);//констрктор с параметром
+    Campaign(unsigned r, vector<double>mas);//конструктор с параметром
+    Campaign(const Campaign &ob);//конструктор копирования
+   ~Campaign(){}
+    void set_campaign(unsigned r,const vector<double>&mas){
+        if(r<=mas.size()){
+            budget=r;
+            results.resize(r);
+            results.assign(mas.begin(),mas.begin()+r);
+        }
+        else{
+            cerr<<"error: budget exceeds results count "<<endl;
+        }
+    }
+    void set_degree(unsigned r){budget=r;};
 
+    unsigned get_budget()const{ return budget;};
+    const vector<double> get_results() const {return results;}
+    //void ROI {}
+};
+
+#endif
---
+++ Svetlikova_LR3-4_Methods.h
@@ -0,0 +1,1 @@
+#include <C:\Users\Chris Gargoyle\Svetlikova_LR3-4_ClassCampaign\Svetlikova_LR3-4ClassCampaign\Svetlikova_LR3-4_ClassCampaign.h>
